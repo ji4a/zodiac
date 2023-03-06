c8603ebc8fd113fc47480cffedc4986181a1a970
ji4a <117786715+ji4a@users.noreply.github.com>
2023-03-07 00:46:53 +0100

    Create getALL.sh

---
+++ SHOWSERVER/getALL.sh
@@ -0,0 +1,15 @@
+curl http://RIP_IP_SRVER_HERE/ram/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/ram/index.txt
+curl http://RIP_IP_SRVER_HERE/stier/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/stier/index.txt
+curl http://RIP_IP_SRVER_HERE/tweelingen/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/tweelingen/index.txt
+
+curl http://RIP_IP_SRVER_HERE/kreeft/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/kreeft/index.txt
+curl http://RIP_IP_SRVER_HERE/leeuw/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/leeuw/index.txt
+curl http://RIP_IP_SRVER_HERE/maagd/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/maagd/index.txt
+
+curl http://RIP_IP_SRVER_HERE/weegschaal/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/weegschaal/index.txt
+curl http://RIP_IP_SRVER_HERE/schorpioen/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/schorpioen/index.txt
+curl http://RIP_IP_SRVER_HERE/boogschutter/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/boogschutter/index.txt
+
+curl http://RIP_IP_SRVER_HERE/steenbok/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/steenbok/index.txt
+curl http://RIP_IP_SRVER_HERE/waterman/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/waterman/index.txt
+curl http://RIP_IP_SRVER_HERE/vissen/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/vissen/index.txt
