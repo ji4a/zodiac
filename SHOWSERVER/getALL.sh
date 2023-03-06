a1598cec81ba0afc180e696b881b54297c6c0071
ji4a <117786715+ji4a@users.noreply.github.com>
2023-03-07 00:56:54 +0100

    Update getALL.sh

--- SHOWSERVER/getALL.sh
+++ SHOWSERVER/getALL.sh
@@ -1,3 +1,19 @@
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/ram
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/stier
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/tweelingen
+
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/kreeft
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/leeuw
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/maagd
+
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/weegschaal
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/schorpioen
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/boogschutter
+
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/steenbok
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/waterman
+mkdir /home/admin/web/zodiac.ssdextreme.nl/public_html/vissen
+
 curl http://RIP_IP_SRVER_HERE/ram/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/ram/index.txt
 curl http://RIP_IP_SRVER_HERE/stier/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/stier/index.txt
 curl http://RIP_IP_SRVER_HERE/tweelingen/index.txt > /home/admin/web/zodiac.ssdextreme.nl/public_html/tweelingen/index.txt
