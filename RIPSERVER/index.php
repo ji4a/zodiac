4a0ac9deef9fe85b92bb13b32b36bd661433eac6
ji4a <117786715+ji4a@users.noreply.github.com>
2023-03-07 01:06:43 +0100

    Create index.php

---
+++ RIPSERVER/index.php
@@ -0,0 +1,35 @@
+<?php // move this file to /var/www/html/index.php 
+
+<html>
+<head>
+<body>
+
+<p style="text-align:center"><a href="/ram/">RAM</a></p>
+
+<p style="text-align:center"><a href="/stier/">STIER</a></p>
+
+<p style="text-align:center"><a href="/tweelingen/">TWEELINGEN</a></p>
+
+<p style="text-align:center"><a href="/kreeft/">KREEFT</a></p>
+
+<p style="text-align:center"><a href="/leeuw/">LEEUW</a></p>
+
+<p style="text-align:center"><a href="/maagd/">MAAGD</a></p>
+
+<p style="text-align:center"><a href="/weegschaal/">WEEGSCHAAL</a></p>
+
+<p style="text-align:center"><a href="/schorpioen/">SCHORPIOEN</a></p>
+
+<p style="text-align:center"><a href="/boogschutter/">BOOGSCHUTTER</a></p>
+
+<p style="text-align:center"><a href="/steenbok/">STEENBOK</a></p>
+
+<p style="text-align:center"><a href="/waterman/">WATERMAN</a></p>
+
+<p style="text-align:center"><a href="/vissen/">VISSEN</a></p>
+
+</body>
+</head>
+</html>
+  
+?>
