b47c77a3d0ad12004fb905bf6f99bb7301633497
ji4a <117786715+ji4a@users.noreply.github.com>
2023-03-08 00:48:00 +0100

    Create ShowOutput.php

---
+++ showserver/ShowOutput.php
@@ -0,0 +1,80 @@
+<?php 
+$servername = "localhost";
+$username = "admin_zodiac";
+$password = "vFlJU4PSrK";
+$database = "admin_zodiac";
+
+try {
+
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // set the PDO error mode to exception
+    // echo "Connected Successfully";
+    
+} catch(PDOException $e) {
+
+    echo "Connection Failed" .$e->getMessage();
+}
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>BOOGSCHUTTER</title>
+  </head>
+  <body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mt-4">
+             
+                    <div class="card-body">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>BOOGSCHUTTER</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    $query = "SELECT * FROM zodiacs ORDER BY id DESC LIMIT 1";
+                                    $statement = $conn->prepare($query);
+                                    $statement->execute();
+
+                                    $statement->setFetchMode(PDO::FETCH_OBJ); //PDO::FETCH_ASSOC
+                                    $result = $statement->fetchAll();
+                                    if($result)
+                                    {
+                                        foreach($result as $row)
+                                        {
+                                            ?>
+                                            <tr>
+                                                <td><?= $row->boogschutter; ?></td>
+                                            </tr>
+                                            <?php
+                                        }
+                                    }
+                                    else
+                                    {
+                                        ?>
+                                        <tr>
+                                            <td colspan="5">No Record Found</td>
+                                        </tr>
+                                        <?php
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+             
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
