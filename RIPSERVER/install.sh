910eb6914f47020c50cc7151b856a6b9d0f2e7a9
ji4a <117786715+ji4a@users.noreply.github.com>
2023-03-07 01:03:31 +0100

    Create install.sh

---
+++ RIPSERVER/install.sh
@@ -0,0 +1,12 @@
+# --------------------------------------------------------------- #
+# INSTALL APACHE AND HTTRACK and BEAUTIFULSOUP BS4 ON UBUNTUN 22.04 
+# --------------------------------------------------------------- #
+
+ sudo apt update
+ sudo apt install apache2
+ sudo ufw app list
+ sudo ufw allow 'Apache'
+ sudo ufw status
+
+ sudo apt-get install httrack webhttrack
+ sudo apt-get install python3-bs4
