0828be98d396139c7e56b45259783cf98f435271
ji4a <117786715+ji4a@users.noreply.github.com>
2023-03-11 20:26:10 +0100

    Update insertDB.php

--- showserver/insertDB.php
+++ showserver/insertDB.php
@@ -14,42 +14,42 @@ $file_ram= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public_html/r
 while(!$file_ram->eof())
 {
  $line=$file_ram->fgets();
-    list($ram)=explode(';',$line);
+    list($ram)=explode('q',$line);
 }
 
 $file_stier= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public_html/stier/index.txt');
 while(!$file_stier->eof())
 {
  $line=$file_stier->fgets();
-    list($stier)=explode(';',$line);
+    list($stier)=explode('q',$line);
 }
 
 $file_tweelingen= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public_html/tweelingen/index.txt');
 while(!$file_tweelingen->eof())
 {
  $line=$file_tweelingen->fgets();
-    list($tweelingen)=explode(';',$line);
+    list($tweelingen)=explode('q',$line);
 }
 
 $file_kreeft= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public_html/kreeft/index.txt');
 while(!$file_kreeft->eof())
 {
  $line=$file_kreeft->fgets();
-    list($kreeft)=explode(';',$line);
+    list($kreeft)=explode('q',$line);
 }
 
 $file_leeuw= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public_html/leeuw/index.txt');
 while(!$file_leeuw->eof())
 {
  $line=$file_leeuw->fgets();
-    list($leeuw)=explode(';',$line);
+    list($leeuw)=explode('q',$line);
 }
 
 $file_maagd= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public_html/maagd/index.txt');
 while(!$file_maagd->eof())
 {
  $line=$file_maagd->fgets();
-    list($maagd)=explode(';',$line);
+    list($maagd)=explode('q',$line);
 }
 
 
@@ -57,14 +57,14 @@ $file_weegschaal= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public
 while(!$file_weegschaal->eof())
 {
  $line=$file_weegschaal->fgets();
-    list($weegschaal)=explode(';',$line);
+    list($weegschaal)=explode('q',$line);
 }
 
 $file_schorpioen= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public_html/schorpioen/index.txt');
 while(!$file_schorpioen->eof())
 {
  $line=$file_schorpioen->fgets();
-    list($schorpioen)=explode(';',$line);
+    list($schorpioen)=explode('q',$line);
 }
 
 
@@ -72,7 +72,7 @@ $file_boogschutter= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/publ
 while(!$file_boogschutter->eof())
 {
  $line=$file_boogschutter->fgets();
-    list($boogschutter)=explode(';',$line);
+    list($boogschutter)=explode('q',$line);
 }
 
 
@@ -80,7 +80,7 @@ $file_steenbok= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public_h
 while(!$file_steenbok->eof())
 {
  $line=$file_steenbok->fgets();
-    list($steenbok)=explode(';',$line);
+    list($steenbok)=explode('q',$line);
 }
 
 
@@ -88,20 +88,21 @@ $file_waterman= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public_h
 while(!$file_waterman->eof())
 {
  $line=$file_waterman->fgets();
-    list($waterman)=explode(';',$line);
+    list($waterman)=explode('q',$line);
 }
 
 $file_vissen= new SplFileObject('/home/admin/web/zodiac.ssdextreme.nl/public_html/vissen/index.txt');
 while(!$file_vissen->eof())
 {
  $line=$file_vissen->fgets();
-    list($vissen)=explode(';',$line);
+    list($vissen)=explode('q',$line);
 }
 
+$newDate = date("d-m-Y");
 
 
 {
-    $sth = $dbh->prepare('INSERT INTO zodiacs values(NULL,?,?,?,?,?,?,?,?,?,?,?,?)');
+    $sth = $dbh->prepare('INSERT INTO zodiacs values(NULL,?,?,?,?,?,?,?,?,?,?,?,?,?)');
     $sth->bindValue(1,$ram,PDO::PARAM_STR);
     $sth->bindValue(2,$stier,PDO::PARAM_STR);
     $sth->bindValue(3,$tweelingen,PDO::PARAM_STR);
@@ -114,6 +115,7 @@ while(!$file_vissen->eof())
     $sth->bindValue(10,$steenbok,PDO::PARAM_STR);
     $sth->bindValue(11,$waterman,PDO::PARAM_STR);
     $sth->bindValue(12,$vissen,PDO::PARAM_STR);
+    $sth->bindValue(13,$newDate,PDO::PARAM_STR);
 
     $sth->execute();
 }
